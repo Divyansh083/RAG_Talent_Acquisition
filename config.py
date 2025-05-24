48afa5c2fca9e9bcbcfd7d33f8677df464dcc46d
Divyansh Gupta <86774214+Divyansh083@users.noreply.github.com>
2025-05-24 15:40:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ WHISPER_MODEL = "medium"
 GROQ_MODEL = "llama3-70b-8192"
 
 # === API Keys ===
-os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = ""
